e7cb27b34a82fcf573d4522ab5cb505615701088
BArsen7 <arseniybabanov25@gmail.com>
2024-10-05 13:05:22 +0300

    Тестовый код парсера с помощью API Вк

---
+++ parse/test-api.py
@@ -0,0 +1,19 @@
+import requests
+
+# переменные 
+TOKEN_USER =  #токен пользователя
+VERSION = 5.131 # используемая версия апи
+DOMAIN =  'mephi_official' #имя сообщества из адресной строки
+
+# через api vk вызываем статистику постов
+response = requests.get('https://api.vk.com/method/wall.get',
+params={'access_token': TOKEN_USER,
+        'v': VERSION,
+        'domain': DOMAIN,
+        'count': 1,
+        'filter': str('owner')})
+
+
+data = response.json() 
+
+print (data)
